288a4d0664847f8e8bbadf2f98e4df2004c6951d
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2026-01-07 23:21:00 +0200

    feat: Add basic form generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,39 @@
-const App = () => <>Form Generator</>;
+import { Container } from "@mui/material";
+import { SetStateAction, useState } from "react";
+import Form from "./components/core/Form";
+import Generator from "./components/core/Generator";
+
+const App = () => {
+  const [jsonData, setJsonData] = useState(
+    JSON.stringify(
+      {
+        fields: [],
+      },
+      null,
+      2
+    )
+  );
+
+  const handleJsonChange = (ev: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setJsonData(ev.target.value);
+  };
+
+  return (
+    <Container
+      sx={{
+        mt: 3,
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        gap: 3,
+      }}
+    >
+      <Generator jsonData={jsonData} handleJsonChange={handleJsonChange} />
+      <Form fields={JSON.parse(jsonData).fields} />
+    </Container>
+  );
+};
+
 export default App;
---
+++ src/components/core/Form/Field.tsx
@@ -0,0 +1,60 @@
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import { IField } from "../../../types";
+
+export const Field = ({ type, label, placeholder, options }: IField) => {
+  switch (type) {
+    case "text":
+    case "textarea":
+      return (
+        <TextField
+          label={label}
+          placeholder={placeholder}
+          fullWidth
+          multiline={type === "textarea"}
+        />
+      );
+    case "dropdown":
+      return (
+        <TextField select label={label} fullWidth defaultValue="">
+          {options?.map((option, i) => (
+            <MenuItem key={i} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      );
+    case "checkbox":
+      return (
+        <FormControl>
+          <FormControlLabel control={<Checkbox />} label={label} />
+        </FormControl>
+      );
+    case "radio":
+      return (
+        <FormControl>
+          <FormLabel>{label}</FormLabel>
+          <RadioGroup>
+            {options?.map((option, i) => (
+              <FormControlLabel
+                key={i}
+                value={option.value}
+                control={<Radio />}
+                label={option.label}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+      );
+    default:
+      return null;
+  }
+};
